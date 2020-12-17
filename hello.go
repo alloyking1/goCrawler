0143ca5d5566e81ca918154d0c894944662f0804
alloyking1 <alloyking1@gmail.com>
2020-12-17 17:48:09 -0500

    added two go packages, flag and os. help me receive argument from command line

--- hello.go
+++ hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"flag"
+	"os"
+)
 
 func main () {
-	fmt.Println("first go programme")
+	flag.Parse()
+	args:= flag.Args()
+
+	if len (args) < 1{
+		fmt.Println("Please provide me a page to craw");
+		os.Exit(1)
+	}
+
 }
